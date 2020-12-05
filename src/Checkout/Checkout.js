7590539ab6584d56170d2dfb82d0998804d21b1e
weversonbdelima <weversonbdelima@gmail.com>
2020-12-04 22:19:31 -0300

    feat: add shipping calculator method

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -11,6 +11,45 @@ class Checkout extends React.Component{
         }
     }
 
+    componentWillMount(){
+        //Calcula o preço total dos produtos do carrinho.
+        this.calculatorTotalPrice();
+        //Calcula o preço do frete
+        this.calculatorShippping();
+    }
+
+    calculatorTotalPrice(){
+        //Carrinho auxiliar recebe uma cópia de carrinho.
+        let cartAux = this.state.cart.slice();
+        let totalPriceAux = 0;
+        //Soma o subtotal dos produtos do carrinho.
+        cartAux.map(product => {
+            totalPriceAux += product.subTotalPrice
+        })
+        //Salva o preço total com os dados atualizados.
+        this.setState({
+            totalPrice: totalPriceAux
+        })
+    }
+
+    calculatorShippping(){
+        //Carrinho auxiliar recebe uma cópia de carrinho.
+        let cartAux = this.state.cart.slice();
+        let totalPriceAux = this.state.totalPrice;
+        let shippingAux = 0;
+        //Se o preço total for acima de 250, o frente é gratis.
+        if(totalPriceAux > 250){
+            shippingAux = 0;
+        }else{
+            //Caso contrário, soma 10,00 ao frente por cada produto adicionado.
+            shippingAux = cartAux.length() * 10;
+        }
+        //Salva o valor do frete com os dados atualizados.
+        this.setState({
+            shipping: shippingAux
+        })
+    }
+
     render(){
         return(
             <View>
