1bc6c022254c3f7193303098c47466aab5ea248c
weversonbdelima <weversonbdelima@gmail.com>
2020-12-05 12:09:16 -0300

    feat: add add product cart method

--- src/Shopping/Shopping.js
+++ src/Shopping/Shopping.js
@@ -13,7 +13,8 @@ class Shopping extends React.Component{
         super(props);
         this.state = {
             products: Products,
-            selectedValue: ''
+            selectedValue: '',
+            cart: []
         }
     }
 
@@ -76,17 +77,37 @@ class Shopping extends React.Component{
                 
             });
         }
-
-        
-
-
         //Atualiza o estado dos produtos e o valor selecionado.
         this.setState({
             products: productsAux,
             selectedValue: productValue
         })
+    }
 
-        console.log(productValue)
+    addProductCart(product){
+        //Carrinho auxiliar recebe carrinho.
+        let cartAux = this.state.cart.slice();
+        let productAux = [];
+        let index = 0;
+        //Verifica se existe algum elemento correspondente no carrinho
+        if(cartAux.indexOf(product) !== -1){
+            //Caso exista elemento no carrinho, incrementa quantidade do elemento.
+            index = cartAux.indexOf(product);
+            productAux = cartAux[index];
+            productAux.quantity += 1;
+            cartAux[index] = productAux;
+        }else{
+            //Se não existir elemento, insere no carrinho.
+            product.quantity = 1;
+            //Calcula o preço subtotal do produto
+            product.priceSubTotal = product.price;
+            cartAux.push(product);
+        
+        }
+        //Salva o carrinho com os dados atualizados
+        this.setState({
+            cart: cartAux
+        });
     }
 
 
